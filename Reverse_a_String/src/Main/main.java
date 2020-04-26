d1ba9c3247568bdd10513b50cddb1e5f46ed58db
John Yu <goldtruffle2015@hotmail.com>
2020-04-26 16:06:27 -0600

    Added user input

--- Reverse_a_String/src/Main/main.java
+++ Reverse_a_String/src/Main/main.java
@@ -1,5 +1,11 @@
 package Main;
 
+import java.util.Scanner; // Import the scanner class
+
 public class main {
-	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); // Create a scanner object
+		System.out.println("Enter text: ");
+		String user_input = scan.nextLine(); // Reads user input
+	}
 }
