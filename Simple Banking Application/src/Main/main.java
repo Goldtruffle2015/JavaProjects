963c3540658d69114b7a6bc2ad1d2bc9e0946ca7
John Yu <goldtruffle2015@hotmail.com>
2020-04-27 19:47:26 -0600

    User can now view the last transaction made as well as leave the program.

--- Simple Banking Application/src/Main/main.java
+++ Simple Banking Application/src/Main/main.java
@@ -9,18 +9,32 @@ public class main {
 		
 		// Menu //
 		user.menu();
-		while (true) {
+		boolean run = true;
+		while (run) {
 			char choice = user.options();
 			System.out.println(); // Blank space
 			// Options //
 			switch(choice) {
-			case 'a':
-			case 'A':
-				user.checkBalance();
-				System.out.println(); // Blank space
-			case 'b':
-			case 'B':
-				user.deposit();
+				case 'a':
+				case 'A':
+					user.checkBalance();
+					System.out.println(); // Blank space
+					break;
+				case 'b':
+				case 'B':
+					user.deposit();
+					break;
+				case 'c':
+				case 'C':
+					user.withdraw();
+					break;
+				case 'd':
+				case 'D':
+					user.disp_last_transaction();
+					break;
+				default:
+					System.out.println("Have a great day.");
+					run = false;
 			}
 		}
 	}
@@ -31,6 +45,7 @@ class bankAccount {
 	private static String userName;
 	private static int userID;
 	private static int balance;
+	private static String last_trans = "No transaction has been made";
 
 	// -- Attributes -- //
 	
@@ -83,6 +98,23 @@ class bankAccount {
 		Scanner scan = new Scanner(System.in);
 		int amt = scan.nextInt();
 		balance = balance + amt;
+		System.out.println(); // Blank space
+		last_trans = "Deposited: $" + amt;
+	}
+	
+	// -- Withdraw -- //
+	public void withdraw() {
+		System.out.println("Enter an amount to withdraw:");
+		Scanner scan = new Scanner(System.in);
+		int amt = scan.nextInt();
+		balance = balance - amt;
+		System.out.println(); // Blank space
+		last_trans = "Withdrawed: $" + amt;
+	}
+	
+	// -- Last Transaction -- //
+	public void disp_last_transaction() {
+		System.out.println(last_trans);
 		System.out.println(); // Blank space
 	}
 }
