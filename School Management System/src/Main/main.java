b7adf819e787bbb8f149c688fdd37d5fbe4a9058
John Yu <goldtruffle2015@hotmail.com>
2020-04-28 18:06:51 -0600

    Added some more methods

--- School Management System/src/Main/main.java
+++ School Management System/src/Main/main.java
@@ -1,8 +1,34 @@
 package Main;
 
+import java.util.Scanner;
+
 public class main {
 	// -- Main -- //
 	public static void main(String[] args) {
+		menu();
+	}
+	
+	// -- Menu -- //
+	public void menu() {
+		System.out.println("Welcome to the school management system.");
+	}
+	
+	public void options() {
+		System.out.println("A. Teacher");
+		System.out.println("B. Student");
+		System.out.println("C. Exit");
+		Scanner scan = new Scanner(System.in);
+		switch (scan.next().charAt(0)) {
+			case 'A':
+			case 'a':
+				System.out.println("A. Add teacher");
+			case 'B':
+			case 'b':
+				
+			case 'c':
+			case 'C':
+				System.exit(1);
+		}
 		
 	}
 }
--- School Management System/src/Main/school.java
+++ School Management System/src/Main/school.java
@@ -6,20 +6,40 @@ public class school {
 	// -- Attributes -- //
 	protected String name; // Name
 	protected int id; // Identification
-	private ArrayList<teacher> teacherList;
-	private ArrayList<student> studentList;
+	private ArrayList<teacher> teacherList; // Stores all instances of teacher
+	private ArrayList<student> studentList; // Stores all instances of student
 	
 	public school(String name, int id) {
 		this.name = name;
 		this.id = id;
 	}
+	// -- Getter -- //
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
 	
 	// -- Methods -- //
 	public void addTeacher(String name, int id) {
-		teacherList.add(new teacher(name, id));
+		this.teacherList.add(new teacher(name, id));
 	}
 	
 	public void addStudent(String name, int id) {
-		studentList.add(new student(name, id));
+		this.studentList.add(new student(name, id));
+	}
+	
+	public void viewTeacherStats() {
+		for (int i=0;i<this.teacherList.size();i++) {
+			System.out.println(this.teacherList.get(i).getName() + " | $" + this.teacherList.get(i).getSalary());
+		}
+	}
+	
+	public void viewStudentStats() {
+		for (int i=0;i<this.studentList.size();i++) {
+			System.out.println(this.studentList.get(i).getName() + " | $" + this.studentList.get(i).getPaid() + " | $" + this.studentList.get(i).getTotal());
+		}
 	}
 }
